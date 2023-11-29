93cc93f999b77340eb5e0abc8ff7d34c326c213a
DESKTOP-E0FAPSB\The user <m0534110618@gmail.com>
2023-11-30 01:14:22 +0200

    commit

---
+++ museum-server-project/Controllers/MuseumController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace museum_server_project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MuseumController 
+    {
+        //static List<Museum> giverList = new List<Museum>(){
+
+        //    new Museum("8:30",0, false) ,
+        //    new Museum("8:40",24, false) ,
+        //    new Museum("8:50",11, false) ,
+            
+            //};
+        public MuseumController()
+        {
+
+        }
+        [Route("GetMuseum")]
+        [HttpGet]
+        public Museum[] GetList()
+        {
+            string jsonFilePath = Path.Combine("Data", "MuseumDb.json");
+
+            string jsonData = File.ReadAllText(jsonFilePath);
+
+            Museum[] data = JsonConvert.DeserializeObject<Museum[]>(jsonData);
+            return data;
+        }
+    
+    }
+}
---
+++ museum-server-project/Museum.cs
@@ -0,0 +1,20 @@
+﻿namespace museum_server_project
+{
+    public class Museum
+    {
+        public string startTime { get; set; }
+        public int availablePlaces { get; set; }
+        public bool isClosed { get; set; }
+
+
+
+        public Museum(string startTime, int availablePlaces, bool isClosed)
+        {
+            this.startTime = startTime;
+            this.availablePlaces = availablePlaces;
+            this.isClosed = isClosed;
+
+        }
+    
+}
+}
